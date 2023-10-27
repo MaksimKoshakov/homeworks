bd8deb5386a4adf23e63e3c2bc0a5a0f3f57fa65
maximkoshakov <makskoshakov@gmail.com>
2023-10-28 00:22:37 +0500

    Add in pom.xml junit

--- src/main/java/de/ait/homework41/Countries.java
+++ src/main/java/de/ait/homework41/Countries.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class Collections {
+public class Countries {
     public static void main(String[] args) {
 
         //ArrayList
